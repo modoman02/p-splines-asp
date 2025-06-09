a8758b5163a8df104dea420497edaecaadd0ce14
Nicolas.Wackermann <nicolas.wackermann@stud.uni-goettingen.de>
2025-06-09 16:29:58 +0200

    changed init() function to a more robust approach

--- R/get_initial_values.R
+++ R/get_initial_values.R
@@ -1,7 +1,8 @@
-get_initial_values <- function (x, y) {
-  initial_model <- lm(y ~ x)
-  y_hat <- fitted.values(initial_model)
-  mu_hat <- rep(mean(y_hat), times = length(y))
-  sigma_hat <- rep(sigma(initial_model), times = length(y))
+get_initial_values <- function(x, y, l, knots) { # ist wohl besser, das alles über Matrix Kalkulation schon mit Spline Design-Matrix zu lösen, da schnellere Konvergenz und noch ein paar Vorteile
+  X <- fit_spline(x = x, knots = knots, l = l)
+  beta <- solve(t(X) %*% X) %*% t(X) %*% y
+  mu_hat <- as.vector(X %*% beta)
+  resid <- abs(y - mu_hat)
+  sigma_hat <- rep(mean(resid), length(y))
   return(list(mu_hat = mu_hat, sigma_hat = sigma_hat))
 }
