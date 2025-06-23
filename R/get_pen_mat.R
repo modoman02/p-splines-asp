246d53ed2d1c2517d4e93b6be3b1ad72e13c46e0
Nicolas.Wackermann <nicolas.wackermann@stud.uni-goettingen.de>
2025-06-23 18:29:32 +0200

    changed the get_pen_mat(), so it returns K = t(D)%*%D and not just D. Also changed the logic by which it calcs the Pen Mat for tp=F a bit.

--- R/get_pen_mat.R
+++ R/get_pen_mat.R
@@ -4,12 +4,11 @@ get_pen_mat <- function(d, r, tp=FALSE) { # d = Anzahl Spalten = Anzahl Basis-Fu
     K <- diag(c(rep(0, l - 1), rep(1, m)))  # Im Kneib Buch steht m-2, ich glaube, es hängt von der Theorie ab, ob man m-2 oder m nimmt, das müssten wir dann im Paper begründen
   }
   else {
-    D1 <- diag(d)
-    D1 <- D1[-1, ] - D1[-nrow(D1), ]  # erste Differenzmatrix mit Dimension d-1 x d
-    K <- D1
-    for (i in 2:r) {
-      K <- K[-1, ] - K[nrow(K), ]
+    D <- diag(d)
+    for (i in 1:r) {
+      D <- D[-1, ] - D[-nrow(D), ]
     }
+    K <- t(D) %*% D
   }
 return (K)
 }
