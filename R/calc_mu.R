232828da763cd4f7bfadd92ee932771f0ce592de
Nicolas.Wackermann <nicolas.wackermann@stud.uni-goettingen.de>
2025-06-23 20:47:25 +0200

    changed the logic mu and sigma are updated with. Now, update_parameters() truly is a wrapper and calc_mu() and calc_simga() return the approximations for the parameters for each iteration

--- R/calc_mu.R
+++ R/calc_mu.R
@@ -1,4 +1,24 @@
 calc_mu <- function(x, y, lambda, beta_hat, ) { # estimate next mu using LRLS
-  mu <- t(x) %*% beta_hat
-  return(mu)
+  for (i in 1:max_iterations_mu) {
+    #  calc score
+    residuals <- y - mu_hat[,i]
+    u_mu <- residuals / sigma_hat[,i]^2
+    W_mu <- diag(1 / sigma_hat[,i]^2, nrow = n, ncol = n)
+    z_mu <- mu[,i] + u_mu / diag(W_mu)
+
+    # update mu
+    beta_hat <- solve(t(X) %*% W_mu %*% X + lambda_mu * K_mu) %*% t(X) %*% W_mu %*% z_mu  # lambda fehlt noch
+    mu_new <- X %*% beta_hat
+
+    mu_hat[,i+1] <- mu_new
+    iterations <- iterations + 1
+    # check convergence
+    GD_mat[i+1] <- calc_deviance(y = y, mu_hat = mu_new, sigma_hat = sigma_new)   # gucken, wie man das wegen Rundungen etd. regelt
+    if (GD_mat[i] < tolerance) {
+      break
+    }
+    else if (iterations == max_iterations) {
+      break
+    }
+  }
 }
--- R/update_parameters.R
+++ R/update_parameters.R
@@ -1,36 +1,39 @@
-update_parameters <- function (x, y, X, Z, lambda_mu, lambda_sigma, tolerance, max_iterations, K_mu, K_sigma) {  # Wrapper, that calls calc_mu() and calc_sigma() iteratively und updates them while doing so. Also calcs residuals and beta after every iteration
-  # initial values for sigma and mu, so the Algorithm can use them as starting points
-  mu_mat <- matrix(0, nrow = length(y), ncol = max_iterations)
-  sigma_mat <- matrix(0, nrow = length(y), ncol = max_iterations)
+update_parameters <- function (y, X, Z, lambda_mu, lambda_sigma, tolerance,
+                               max_iterations, K_mu, K_sigma, max_iterations_mu = 10, max_iterations_sigma = 10) {  # updates mu and sigma respectively, each time with an updated value of the other parameter
+  n <- length(y)
+  mu_hat <- matrix(0, nrow = n, ncol = max_iterations)
+  sigma_hat <- matrix(0, nrow = n, ncol = max_iterations)
+  init_vals <- get_initial_values(X = X, Z = Z, y = y)
   GD_mat <- matrix(0, nrow = 1, ncol = max_iterations)
-  for (i in 1:length(y)) {
-    W_hat[i,i] <- 1/sigma_hat[i]
-  }
-  beta_hat <- solve(t(X) %*% W_hat %*% X) %*% t(X) %*% W_hat %*% y
-  iterations <- 0
+  mu_hat[,1] <- init_vals$mu_hat
+  sigma_hat[,1] <- init_vals$sigma_hat
+
   for (i in 1:max_iterations) {
-    # update mu
-    beta_hat <- solve(t(X) %*% K_mu %*% X) %*% t(X) %*% K_mu %*% y  # Gewichtung der K-Matrix über lambda fehlt noch
-    mu_new <- calc_mu(Z, beta_hat)
-    residuals <- y - mu_new
-    s_hat <- log(abs(residuals)) + 0.635  # adding an additional very small number can prevent errors from happening in case we get a perfect fit (residuals = 0)
-    mu_mat[, i] <- mu_new
-    # update sigma
-    gamma_hat <- solve(t(Z) %*% Z) %*% t(Z) %*% s_hat
-    sigma_new <- calc_sigma(X, gamma_hat)
-    sigma_mat[, i] <- sigma_new
-
-
-    iterations <- iterations + 1
-    # check convergence
-    GD_mat[i] <- calc_deviance(y = y, mu_hat = mu_new, sigma_hat = sigma_new)
-    if (GD_mat[i] < tolerance) {
-      break
-    }
-    else if (iterations == max_iterations) {
-      break
-    }
+  # iterative updates for sigma and mu respectively
+    calc_mu()
   }
+  # for (i in 1:max_iterations_mu) {
+  #   #  calc score
+  #   residuals <- y - mu_hat[,i]
+  #   u_mu <- residuals / sigma_hat[,i]^2
+  #   W_mu <- diag(1 / sigma_hat[,i]^2, nrow = n, ncol = n)
+  #   z_mu <- mu[,i] + u_mu / diag(W_mu)
+  #
+  #   # update mu
+  #   beta_hat <- solve(t(X) %*% W_mu %*% X + lambda_mu * K_mu) %*% t(X) %*% W_mu %*% z_mu  # lambda fehlt noch
+  #   mu_new <- X %*% beta_hat
+  #
+  #   mu_hat[,i+1] <- mu_new
+  #   iterations <- iterations + 1
+  #   # check convergence
+  #   GD_mat[i+1] <- calc_deviance(y = y, mu_hat = mu_new, sigma_hat = sigma_new)   # gucken, wie man das wegen Rundungen etd. regelt
+  #   if (GD_mat[i] < tolerance) {
+  #     break
+  #   }
+  #   else if (iterations == max_iterations) {
+  #     break
+  #   }
+  # }
   return(list(mu_hat = mu_new,
               sigma_hat = sigma_new,
               iterations = iterations,
