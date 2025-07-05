f54d4f4e34d0a37d7b42ee5a245d22eee016a579
Nicolas.Wackermann <nicolas.wackermann@stud.uni-goettingen.de>
2025-07-06 01:20:17 +0200

    calc_sigma() now takes a slightly easier approach to calc z and W. Might wanna change it back later on, but for now it works like this at least

--- R/calc_sigma.R
+++ R/calc_sigma.R
@@ -1,20 +1,20 @@
-calc_sigma <- function (Z, y, K_sigma, sigma_init, mu_hat, lambda_grid, lambda_sigma, max_iterations_sigma, tolerance) { # calc next sigma using Fisher Updates
+calc_sigma <- function (Z, y, K_sigma, sigma_init, mu_hat, lambda_grid, lambda_sigma = 1, max_iterations_sigma, tolerance) { # calc next sigma using Fisher Updates
   n <- length(y)
+  epsilon <- 1e-6
   sigma_hat <- matrix(NA, nrow = n, ncol = max_iterations_sigma + 2)
   sigma_hat[, 1] <- sigma_init
   GD_mat <- numeric(max_iterations_sigma + 2)
   for (i in 1:max_iterations_sigma) {
     # calc score
-    residuals <- y - mu_hat
-    u_sigma <- residuals^2 / sigma_hat[,i]^2 - 1
-    W_sigma <- diag(1 / sigma_hat[,i]^2, nrow = n, ncol = n)
+    u_sigma <- ((y-mu_hat)^2-sigma_hat[,i]^2)/sigma_hat[,i]^2
+    w_sigma <- as.numeric(u_sigma^2)
+    z_sigma <- as.matrix((log(sigma_hat[,i]) + u_sigma^(-1)), ncol = 1)
+    W_sigma <- w_sigma * diag(length(w_sigma))
 
-    # calc pseudo response z
-    z_sigma <- log(sigma_hat[,i]) + u_sigma * sigma_hat[,i]^2
     # update sigma
     gamma_hat <- solve(t(Z) %*% W_sigma %*% Z + lambda_sigma * K_sigma) %*% t(Z) %*% W_sigma %*% z_sigma
     sigma_hat[,i + 1] <- exp(Z %*% gamma_hat)
-
+    sigma_hat[,i + 1][is.na(sigma_hat[,i + 1]) | sigma_hat[,i + 1] < epsilon] <- epsilon
     # check convergence
     GD_mat[i+1] <- calc_deviance(y = y, mu_hat = mu_hat, sigma_hat = sigma_hat[,i+1])   # gucken, wie man das wegen Rundungen etc. regelt
     if (i > 1 && abs(GD_mat[i+1] - GD_mat[i]) < tolerance) {
@@ -28,7 +28,8 @@ calc_sigma <- function (Z, y, K_sigma, sigma_init, mu_hat, lambda_grid, lambda_s
     lambda <- lambda_grid[j]
     gamma_hat_lambda <- solve(t(Z) %*% W_sigma %*% Z + lambda * K_sigma) %*% t(Z) %*% W_sigma %*% z_sigma
     sigma_hat_lambda <- exp(Z %*% gamma_hat_lambda)
-    S_lambda <- Z %*% solve(t(Z) %*% W_sigma %*% Z + lambda * K_sigma) %*% t(Z) %*% W_sigma
+    W_sigma_lambda <- diag(as.numeric(1 / sigma_hat_lambda^2), nrow = n)
+    S_lambda <- Z %*% solve(t(Z) %*% W_sigma_lambda %*% Z + lambda * K_sigma) %*% t(Z) %*% W_sigma
     residuals_lambda <- z_sigma - log(sigma_hat_lambda)
     score[j] <- mean(residuals_lambda^2) / (1 - mean(diag(S_lambda)))^2
   }
